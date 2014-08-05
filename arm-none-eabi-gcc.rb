8417658227babe5c38ec030128d658c92763eb70
Sean Nelson <audiohacked@gmail.com>
2014-08-05 15:04:47 -0600

    add arm-none-eabi-gcc 4.8.3

---
+++ arm-none-eabi-gcc.rb
@@ -0,0 +1,42 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
+#                /usr/local/Library/Contributions/example-formula.rb
+
+class ArmNoneEabiGcc < Formula
+  homepage "https://gcc.gnu.org/"
+  url "http://ftp.gnu.org/gnu/gcc/gcc-4.8.3/gcc-4.8.3.tar.bz2"
+  sha1 "da0a2b9ec074f2bf624a34f3507f812ebb6e4dce"
+
+  #depends_on 'libiconv'
+  depends_on 'gmp'
+  depends_on 'libmpc'
+  depends_on 'mpfr'
+  depends_on 'arm-none-eabi-binutils'
+  
+  def install
+    target = 'arm-none-eabi'
+
+    args = [
+      "--target=#{target}",
+      "--prefix=#{prefix}",
+      "--infodir=#{info}",
+      "--mandir=#{man}",
+      "--disable-nls",
+      "--enable-languages=c,c++",
+      "--without-headers"
+    ]
+
+    mkdir 'build' do
+      system "../configure", *args
+      system "make all-gcc"
+      system "make all-target-libgcc"
+      ENV.deparallelize
+      system "make install-gcc"
+      system "make install-target-libgcc"
+    end
+    #info.rmtree
+    #man7.rmtree
+  end
+
+end
